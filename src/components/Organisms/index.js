82d14c7cabcef2d75b37630a21b277a9637de2fa
Jordi <jeroig@idonia.com>
2020-11-06 22:59:00 +0100

    remove feedback fish import

--- src/components/Organisms/index.js
+++ src/components/Organisms/index.js
@@ -1,11 +1,10 @@
 import ContactForm from './ContactForm.vue'
-import FeedbackFish from './FeedbackFish.vue'
 import Modal from './Modal.vue'
 
 export default {
-  ContactForm, FeedbackFish, Modal
+  ContactForm, Modal
 }
 
 export {
-  ContactForm, FeedbackFish, Modal
+  ContactForm, Modal
 }
