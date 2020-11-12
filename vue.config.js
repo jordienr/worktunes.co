1bdf49deb5a2954e5e770f4397b4a8f63ab700ab
Jordi Enric <je.roig@innovation.es>
2020-11-12 10:58:19 +0100

    fix manifest.json pwa config

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,13 @@ module.exports = {
           prependData: `@import "@/assets/scss/styles.scss";`
         }
       }
-    } 
+    },
+    pwa: {
+      name: 'Worktunes',
+      themeColor: '#1a212d',
+      msTileColor: '#1a212d',
+      appleMobileWebAppCapable: 'yes',
+      appleMobileWebAppStatusBarStyle: '#1a212d',
+    }
   }
   
